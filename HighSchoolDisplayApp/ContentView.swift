7bddc241f1de4d1345204fa5ea0a7301f24b500d
Zak M <150296130+ISLE-MEDIA@users.noreply.github.com>
2024-01-25 09:32:01 -0500

    Initial Commit

---
+++ HighSchoolDisplayApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HighSchoolDisplayApp
+//
+//  Created by Zak Mills on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HighSchoolDisplayApp/HighSchoolDisplayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighSchoolDisplayAppApp.swift
+//  HighSchoolDisplayApp
+//
+//  Created by Zak Mills on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct HighSchoolDisplayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
